736148006b1e7017a6f4a329c6be6e80091a4491
Mickaya <sermont.mickael>
2018-12-23 19:39:22 +0330

    Add jquery 1.X and add div overlay navigation for mobiles

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+$( ".toggleNavbar" ).click(function() {
+    $( "#navOverlay" ).fadeToggle( "slow", "linear" );
+});
+
+
+
+// $(document).ready(function() {
+// $('.open-menu').on('click', function() {
+//     $('.overlay').addClass('open');
+// });
+
+// $('.close-menu').on('click', function() {
+//     $('.overlay').removeClass('open');
+// });
+// });
